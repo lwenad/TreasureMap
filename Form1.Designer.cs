8c677cd734f7c0e4c11ff0a28a406a9abffaa5e0
Lei Wen <lei.wen@outlook.com>
2021-01-23 23:22:47 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace TreasureMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.HintButton = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.CheckBoxPassword = new System.Windows.Forms.CheckBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.HintButton);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 22);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label2.Location = new System.Drawing.Point(295, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // HintButton
+            // 
+            this.HintButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.HintButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.HintButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HintButton.ForeColor = System.Drawing.Color.Black;
+            this.HintButton.Location = new System.Drawing.Point(709, 0);
+            this.HintButton.Name = "HintButton";
+            this.HintButton.Size = new System.Drawing.Size(75, 22);
+            this.HintButton.TabIndex = 1;
+            this.HintButton.Text = "Hints";
+            this.HintButton.UseVisualStyleBackColor = false;
+            this.HintButton.Visible = false;
+            this.HintButton.Click += new System.EventHandler(this.HintButton_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(295, 22);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.CheckBoxPassword);
+            this.panel2.Controls.Add(this.maskedTextBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 44);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(784, 21);
+            this.panel2.TabIndex = 1;
+            // 
+            // CheckBoxPassword
+            // 
+            this.CheckBoxPassword.AutoSize = true;
+            this.CheckBoxPassword.Dock = System.Windows.Forms.DockStyle.Right;
+            this.CheckBoxPassword.Location = new System.Drawing.Point(607, 0);
+            this.CheckBoxPassword.Name = "CheckBoxPassword";
+            this.CheckBoxPassword.Size = new System.Drawing.Size(102, 21);
+            this.CheckBoxPassword.TabIndex = 3;
+            this.CheckBoxPassword.Text = "Show Password";
+            this.CheckBoxPassword.UseVisualStyleBackColor = true;
+            this.CheckBoxPassword.Click += new System.EventHandler(this.CheckBoxPassword_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.BackColor = System.Drawing.Color.White;
+            this.maskedTextBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.maskedTextBox1.Location = new System.Drawing.Point(295, 0);
+            this.maskedTextBox1.Mask = "1";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.PasswordChar = '*';
+            this.maskedTextBox1.Size = new System.Drawing.Size(285, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(709, 0);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(1);
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(1);
+            this.label1.Size = new System.Drawing.Size(295, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter password to access the treasure map";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(784, 896);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.AutoScroll = true;
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 65);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(784, 896);
+            this.panel3.TabIndex = 1;
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(784, 22);
+            this.TopPanel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 961);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.TopPanel);
+            this.MaximumSize = new System.Drawing.Size(800, 1000);
+            this.MinimumSize = new System.Drawing.Size(800, 1000);
+            this.Name = "Form1";
+            this.Text = "Treasure Map V1.0";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button HintButton;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.CheckBox CheckBoxPassword;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+
+namespace TreasureMap
+{
+    public partial class Form1 : Form
+    {
+
+        List<Image> ListErrorImage = new List<Image>();
+        Image TreasureMapImage = Image.FromFile("C:\\Users\\Lei\\Pictures\\treasure_map.png");
+        private static readonly string Password = "1983creamer";
+        private static int _errorCounter = 0;
+        private static DateTime? LastClickTime = null;
+        private static int WaitInSecond = 0;
+        private static readonly string FailedAttmepsText = "Failed attempts {0}";
+        private static SoundPlayer SoundPlayer = new SoundPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+
+            ListErrorImage.Add(Image.FromFile("C:\\Users\\Lei\\Pictures\\ErrorImage1.PNG"));
+            ListErrorImage.Add(Image.FromFile("C:\\Users\\Lei\\Pictures\\ErrorImage2.PNG"));
+            ListErrorImage.Add(Image.FromFile("C:\\Users\\Lei\\Pictures\\ErrorImage3.PNG"));
+            ListErrorImage.Add(Image.FromFile("C:\\Users\\Lei\\Pictures\\ErrorImage4.PNG"));
+            ListErrorImage.Add(Image.FromFile("C:\\Users\\Lei\\Pictures\\ErrorImage5.jpg"));
+
+
+            label2.Text = FailedAttmepsText.Replace("{0}", "0");
+            maskedTextBox1.Focus();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(LastClickTime == null)
+            {
+                LastClickTime = DateTime.Now;
+            }
+            else
+            {
+                if(DateTime.Now.AddSeconds(-1* WaitInSecond) < LastClickTime)
+                {
+                    MessageBox.Show($"too fast, have to wait at least {WaitInSecond} seconds to enter password again!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                else
+                {
+                    LastClickTime = DateTime.Now;
+
+                }
+            }
+
+            try
+            {
+                if (string.IsNullOrEmpty(maskedTextBox1.Text) || maskedTextBox1.Text.ToLower() != Password)
+                {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
+                    Random random = new Random();
+                    int randomNumber = random.Next(0, 100);
+                    int index = randomNumber % ListErrorImage.Count;
+                    pictureBox1.Image = ListErrorImage[index];
+                    _errorCounter++;
+
+                    label2.Text = FailedAttmepsText.Replace("{0}", _errorCounter.ToString());
+
+                    if (_errorCounter == 10 )
+                    {
+                        HintButton.Visible = true;
+                    }
+                }
+                else
+                {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    pictureBox1.Image = TreasureMapImage;
+                }
+            }
+             catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void HintButton_Click(object sender, EventArgs e)
+        {
+            string hint1 = "hint1: the last name is very interesting ...";
+            string hint2 = "hint2: English is the second language, how should i spelling the last name ? ";
+            string hint3 = "hint3  I need to add more creamer for the coffee.";
+
+            string result = null;
+            if (_errorCounter >= 10)
+            {
+                result = hint1;
+            }
+
+            if (_errorCounter > 15)
+            {
+                result = result+ "\n" + hint2;
+            }
+            if (_errorCounter > 20)
+            {
+                result = result + "\n" + hint3;
+            }
+
+            MessageBox.Show(result);
+        }
+
+        private void CheckBoxPassword_Click(object sender, EventArgs e)
+        {
+
+            if(CheckBoxPassword.Checked)
+            {
+                maskedTextBox1.PasswordChar = '\0';
+            }
+            else
+            {
+                maskedTextBox1.PasswordChar = '*';
+            }
+        }
+    }
+}
